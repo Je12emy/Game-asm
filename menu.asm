7ce244ac796a50add1977f6255a0635baa8d39ed
Jeremy <jeremyzelaya@hotmail.es>
2019-04-28 01:59:43 -0600

    Merged juego into menu
    Merged the program for the game characters written by @pablocarmona29  into the main program. [raname due soon]
    
    Co-Authored-By: pablocarmona29 <pablocarmona29@users.noreply.github.com>

--- menu.asm
+++ menu.asm
@@ -2,10 +2,91 @@
  include carriles.asm 
  include carretera.asm
  
+;Macros especiales para los personajes del juego
+
+CUADRO MACRO X,Y,COLOR,TAMANO
+    
+    LOCAL   FILA,COLUMNA,FILA2,COLUMNA2
+    XOR     DX,DX        
+    MOV     BX,TAMANO
+    MOV     NUMERO,BX
+    
+    MOV     BX,X
+    MOV     XAUX,BX
+    
+    MOV     BX,Y
+    MOV     YAUX,BX
+    
+FILA:
+    PUNTO   XAUX,YAUX,COLOR
+    INC     XAUX
+    DEC     NUMERO
+    JNZ     FILA
+    
+    DEC     XAUX    
+    INC     YAUX
+    MOV     BX,TAMANO
+    MOV     NUMERO,BX
+    
+COLUMNA:
+
+    PUNTO   XAUX,YAUX,COLOR
+    INC     YAUX
+    DEC     NUMERO
+    JNZ     COLUMNA
+    
+    DEC     XAUX
+    DEC     YAUX
+    MOV     BX,TAMANO
+    MOV     NUMERO,BX
+
+FILA2:
+    
+    PUNTO   XAUX,YAUX,COLOR
+    DEC     XAUX
+    DEC     NUMERO
+    JNZ     FILA2
+    
+    INC     XAUX
+    DEC     YAUX
+    MOV     BX,TAMANO
+    MOV     NUMERO,BX
+    
+COLUMNA2:    
+    
+    PUNTO   XAUX,YAUX,COLOR
+    DEC     YAUX
+    DEC     NUMERO
+    JNZ     COLUMNA2
+    INC     XAUX
+    INC     YAUX
+
+ENDM 
+
+RETARDO MACRO   VELOCIDAD
+    
+        LOCAL   RETARDO1,RETARDO2    
+        PUSH    CX
+        MOV 	CX,  VELOCIDAD	
+RETARDO1:
+        MOV 	BX,  0FH
+RETARDO2:
+        DEC 	BX
+        JNZ 	RETARDO2
+        LOOP 	RETARDO1
+        POP     CX        
+ENDM
+ 
+ 
+ 
+ 
+ 
+ 
+ 
 .MODEL SMALL
 .STACK 100H
 .DATA
-    ;Prueba de GitHub
+    ;Parametros para el menu
     iniciar db "[1] INICIAR EL JUEGO.$"
     marcador db "[2] VER MARCADORES.$"
     salir db   "[3] SALIR.$"
@@ -13,6 +94,7 @@
     error db "NO HA INGRESADO UNA DE LAS OPCIONES!$" 
     precione db "[PRECIONE UNA TECLA]$"
     puntero db ":"
+    
     x dw "$"
     y dw "$" 
     
@@ -24,6 +106,24 @@
     grosor db "$"
     fondo db "$"
     ENTER   DB      0AH,0DH,'$'
+    
+    
+    ;xx      dw 1
+    ;yy      dw 1
+        
+    XAUX    DW 0
+    YAUX    DW 0
+        
+    ;X       dw 319
+    ;Y       dw 10  
+        
+    XCAR    DW 0
+    YCAR    DW 0 
+        
+    MEN1    DB 'GAME OVER','$' 
+               
+    NUMERO      DW 0 
+    VELOCIDAD   DW 0FFFH
 
 .CODE
 
@@ -88,7 +188,7 @@ opciones:
      
      
     juego:
-    
+    ;Dibujo del campo de juego
     
 
     MOV AH,00
@@ -119,16 +219,143 @@ opciones:
 
                  
     LINEASALTEADA x,y,xx,yy
-   
-   
+    
+    ;Dibujo de los personajes de juego.
+    
+        MOV X,319
+        MOV Y,10
         
+    
+        MOV     XCAR,219
+        MOV     YCAR,220
         
+        CUADRO  XCAR,YCAR,0FH,20        
+
+        MOV     X,219
+        MOV     Y,10
+        
+        CUADRO  X,Y,04H,20
+        
+        MOV     XX,421
+        MOV     YY,150
+        
+        CUADRO  XX,YY,04H,20  
+        
+        ; reset mouse and get its status:
+        mov     ax, 0
+        int     33h
+        
+        ; display mouse cursor:
+        mov     ax, 1
+        int     33h
+        
+check_mouse_buttons:
+        
+        MOV     DX,Y
+        ADD     DX,20
+        CMP     YCAR,DX
+        JE      VALIDARXL 
+        
+        MOV     DX,YY
+        ADD     DX,20
+        CMP     YCAR,DX
+        JE      VALIDARXXL       
+
+SEGUIR:                                                
+        mov     ax, 3
+        int     33h        
+        
+        cmp     bx, 1  
+        je      LEFT
+                  
+        cmp     bx, 2  
+        je      RIGHT
+        
+        cmp     bx, 3  
+        je      Halt        
+        
+        RETARDO VELOCIDAD
+        
+        CUADRO  X,Y,08H,20 ;BORRAR
+        ADD     Y,10 
+        CUADRO  X,Y,04H,20 
+        
+        CUADRO  XX,YY,08H,20 ;BORRAR
+        ADD     YY,10
+        CUADRO  XX,YY,04H,20        
+        
+        CMP     Y,460
+        JE      VOLVERINICIO 
+        
+        CMP     YY,460
+        JE      VOLVERINICIO2 
+                                                
+        jmp check_mouse_buttons
+
+VALIDARXL:
+
+        MOV     DX,X
+        CMP     XCAR,DX
+        JE      GAMEOVER    
+        jmp     SEGUIR 
+        
+VALIDARXXL:
+
+        MOV     DX,XX
+        CMP     XCAR,DX
+        JE      GAMEOVER    
+        jmp     SEGUIR        
+
+VOLVERINICIO:
+        ;CUADRO  X,Y,00H,20 ;BORRAR
+        CUADRO  X,Y,08H,20 ;BORRAR
+        MOV     Y,10
+        SUB     VELOCIDAD,96H        
+        jmp     check_mouse_buttons 
+               
+VOLVERINICIO2:
+        
+        CUADRO  XX,YY,08H,20 ;BORRAR
+        MOV     YY,10
+        jmp     check_mouse_buttons 
+
+LEFT:
+        
+        CUADRO  XCAR,YCAR,08H,20
+        MOV     XCAR,219
+        MOV     YCAR,220        
+        CUADRO  XCAR,YCAR,0FH,20
+        JMP     check_mouse_buttons  
+        
+RIGHT: 
+        
+        CUADRO  XCAR,YCAR,08H,20
+        MOV     XCAR,421
+        MOV     YCAR,220        
+        CUADRO  XCAR,YCAR,0FH,20
+        JMP     check_mouse_buttons  
+
+GAMEOVER:
+
+        LEA     DX,MEN1
+        MOV	    AH,9
+        INT     21H
+    
+            
     call readkey()
     
     
-halt:
-    mov ah,4ch
-    int 21h
+HALT:        
+
+        MOV AH,01H
+        INT 21H
+        
+        MOV AH,00H
+        MOV AL,03H
+        INT 10H
+
+        mov ah,4ch
+        int 21h 
     
 comparar proc
 mayor1:    
